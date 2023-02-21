2df730a8114a103723e2d56302993ed379c5cf57
bloxster <40316187+bloxster@users.noreply.github.com>
2023-02-21 14:02:06 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Silkworm'
+copyright = '2023, Apache-2.0 license'
+author = 'Bloxster'
 
 release = '0.1'
 version = '0.1.0'
